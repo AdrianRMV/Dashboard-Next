8e15eb9674844eab1646959c3e9f2d03f409b222
AdrianRMV <adrianthelawx@gmail.com>
2023-11-30 20:01:02 -0700

    Add User Page Design Done

--- app/dashboard/products/add/page.jsx
+++ app/dashboard/products/add/page.jsx
@@ -1,6 +1,29 @@
 import styles from '@/app/ui/dashboard/products/addProduct/addProduct.module.css';
 const AddProductPage = () => {
-    return <div className={styles.container}>AddProductPage</div>;
+    return (
+        <div className={styles.container}>
+            <form action="" className={styles.form}>
+                <input type="text" placeholder="Title" name="title" required />
+                <select name="cat" id="cat">
+                    <option value="general">Choose a Category</option>
+                    <option value="kitchen">Kitchen</option>
+                    <option value="phone">Phone</option>
+                    <option value="computer">Computer</option>
+                </select>
+                <input type="number" name="price" placeholder="Price" />
+                <input type="number" name="price" placeholder="Stock" />
+                <input type="text" name="color" placeholder="Color" />
+                <input type="text" name="size" placeholder="Size" />
+                <textarea
+                    name="desc"
+                    id="desc"
+                    rows="16"
+                    placeholder="Description"
+                ></textarea>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
 };
 
 export default AddProductPage;

4d6e119dd1ce93542843281e7a8214159ced3f3d
AdrianRMV <adrianthelawx@gmail.com>
2023-12-03 23:48:54 -0700

    Redirect from homepage to dashboard

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,15 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 const Homepage = () => {
-    return <div>Homepage</div>;
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push('/dashboard');
+    }, []);
+
+    return <div>Redireccionando a Dashboard...</div>;
 };
 
 export default Homepage;
